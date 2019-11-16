def1deb93030258d272ab606a55f692ac7ca0715
Matti Mäki-Kihniä <matti.maki-kihnia@epassi.fi>
2019-11-16 14:01:45 +0200

    Prettify

--- android/app/src/main/java/com/github/harmittaa/junction2019/MainActivity.kt
+++ android/app/src/main/java/com/github/harmittaa/junction2019/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         db = FirebaseFirestore.getInstance()
         populateOveralls()
         setupRecyclerView()
+        blocker.bringToFront()
     }
 
     private fun populateOveralls() {
